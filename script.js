a2cc1d7843da09d6b766d398ce00f2e20807a56f
Martynas Baltutis <66354349+Maruchan39@users.noreply.github.com>
2021-02-03 23:20:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 function telephoneCheck(str) {
-    let formatOne = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
-    let formatTwo = /^\([0-9]{3}\)[0-9]{3}-[0-9]{4}$/;
-    let formatThree = /^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$/;
-    let formatFour = /^[0-9]{3} [0-9]{3} [0-9]{4}$/;
-    let formatFive = /^5[0-9]{9}/;
-    let formatSix = /^1[\s-]{0,2}?(\([0-9]{3}\)|[0-9]{3})[\s-]{0,2}?(\([0-9]{3}\)|[0-9]{3})[\s-]{1}?(\([0-9]{4}\)|[0-9]{4})$/;
-    let formats = [formatOne, formatTwo, formatThree, formatFour, formatFive, formatSix];
-    for (let i = 0; i < formats.length; i++) {
-        if (formats[i].test(str)) {
-            return true;
-        }
+  let formatOne = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+  let formatTwo = /^\([0-9]{3}\)[0-9]{3}-[0-9]{4}$/;
+  let formatThree = /^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$/;
+  let formatFour = /^[0-9]{3} [0-9]{3} [0-9]{4}$/;
+  let formatFive = /^5[0-9]{9}/;
+  let formatSix = /^1[\s-]{0,2}?(\([0-9]{3}\)|[0-9]{3})[\s-]{0,2}?(\([0-9]{3}\)|[0-9]{3})[\s-]{1}?(\([0-9]{4}\)|[0-9]{4})$/;
+  let formats = [
+    formatOne,
+    formatTwo,
+    formatThree,
+    formatFour,
+    formatFive,
+    formatSix,
+  ];
+  for (let i = 0; i < formats.length; i++) {
+    if (formats[i].test(str)) {
+      return true;
     }
+  }
 
-    return false;
-
+  return false;
 }
+
 
